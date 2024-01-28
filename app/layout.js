a3a79e8275385829912056a8b1a4df0e8ca65ffb
xbiwas <biwas2059@gmail.com>
2024-01-28 14:36:09 +0545

    updated

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: {
-    default: "Next.js 14 Homepage",
+    default: "Story",
     template: "%s | Next.js 14",
   },
   description: "Next.js starter app description",
