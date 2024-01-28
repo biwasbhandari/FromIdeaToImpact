1526692fa898aee60bbb3fea9d46ede806617305
xbiwas <biwas2059@gmail.com>
2024-01-28 16:36:59 +0545

    deployment debugged

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -6,7 +6,7 @@ import { getPost } from "@/lib/data";
 
 // FETCH DATA WITH AN API
 const getData = async (slug) => {
-  const res = await fetch(`http://localhost:3000/api/blog/${slug}`);
+  const res = await fetch(`${process.env.HOST}/api/blog/${slug}`);
 
   if (!res.ok) {
     throw new Error("Something went wrong");
