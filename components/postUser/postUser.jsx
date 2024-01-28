172a8cb332d9b85a34bf5f1c1139dfb73f679a70
xbiwas <biwas2059@gmail.com>
2024-01-28 20:24:11 +0545

    final commit

--- components/postUser/postUser.jsx
+++ components/postUser/postUser.jsx
@@ -24,7 +24,7 @@ const PostUser = async ({ userId }) => {
     <div className={styles.container}>
       <Image
         className={styles.avatar}
-        src={user.img ? user.img : "/noavatar.png"}
+        src={"/noavatar.png"}
         alt=""
         width={50}
         height={50}
